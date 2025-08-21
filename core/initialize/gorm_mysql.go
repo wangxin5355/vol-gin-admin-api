47c792b93feeacad32bca58fc2d475e2580d8152
wangbenchi66 <69945864@qq.com>
2025-08-21 16:38:19 +0800

    数据库命名全局忽略

--- core/initialize/gorm_mysql.go
+++ core/initialize/gorm_mysql.go
@@ -3,7 +3,6 @@ package initialize
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/wangxin5355/vol-gin-admin-api/config"
-	"github.com/wangxin5355/vol-gin-admin-api/core/initialize/internal"
 	"github.com/wangxin5355/vol-gin-admin-api/global"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -50,7 +49,7 @@ func GormMysqlByConfig(m config.Mysql) *gorm.DB {
 		DefaultStringSize:         191,     // string 类型字段的默认长度
 		SkipInitializeWithVersion: false,   // 根据版本自动配置
 	}
-	if db, err := gorm.Open(mysql.New(mysqlConfig), internal.Gorm.Config(m.Prefix, m.Singular)); err != nil {
+	if db, err := gorm.Open(mysql.New(mysqlConfig), Config(m.Prefix, m.Singular)); err != nil {
 		panic(err)
 	} else {
 		db.InstanceSet("gorm:table_options", "ENGINE=InnoDB")
